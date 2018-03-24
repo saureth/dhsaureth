332b04dbaf11e6c6f0543b59d8ee353614d5339c
dojonode <santiago.sanmartin@udea.edu.co>
2018-03-24 09:44:08 -0500

    Fixing grammar on the body string

--- index.php
+++ index.php
@@ -3,6 +3,6 @@
 		<title> DOJO HEROKU </title>
 	</head>
 	<body>
-		<?php echo '<p>Primer acercamiento a Paas con Heroku</p>'; ?>
+		<?php echo '<p>Primer acercamiento a PaaS con Heroku</p>'; ?>
 	</body>
 </html>
